42d27c81ef9e99e10bf749beeb959541f92f49b3
Marc Neudert <marc.neudert@gmail.com>
2015-10-21 20:26:49 +0200

    adds tests for basic plugin behaviour

---
+++ test/unit/vagrant/devcommands/command_spec.rb
@@ -0,0 +1,45 @@
+require_relative '../../spec_helper'
+
+describe VagrantPlugins::DevCommands::Command do
+  describe 'without a Commandfile' do
+    before :context do
+      @olddir = Dir.pwd
+      @newdir = File.join(File.dirname(__FILE__),
+                          '../../fixtures/missing-commandfile')
+
+      Dir.chdir @newdir
+    end
+
+    it 'displays warning' do
+      env = Vagrant::Environment.new(cwd: @newdir)
+      cmd = described_class.new([], env)
+
+      expect { cmd.execute }.to output(/missing.+Commandfile/i).to_stdout
+    end
+
+    after :context do
+      Dir.chdir(@olddir)
+    end
+  end
+
+  describe 'with an empty Commandfile' do
+    before :context do
+      @olddir = Dir.pwd
+      @newdir = File.join(File.dirname(__FILE__),
+                          '../../fixtures/empty-commandfile')
+
+      Dir.chdir @newdir
+    end
+
+    it 'displays help' do
+      env = Vagrant::Environment.new(cwd: @newdir)
+      cmd = described_class.new([], env)
+
+      expect { cmd.execute }.to output(/no commands/i).to_stdout
+    end
+
+    after :context do
+      Dir.chdir(@olddir)
+    end
+  end
+end
