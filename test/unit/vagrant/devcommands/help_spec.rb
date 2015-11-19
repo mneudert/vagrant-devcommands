55d030909b715af75fe5dc53461e4f0b7b03b23e
Marc Neudert <marc.neudert@gmail.com>
2015-11-19 16:19:43 +0100

    removes usage of command definer from help spec

--- test/unit/vagrant/devcommands/help_spec.rb
+++ test/unit/vagrant/devcommands/help_spec.rb
@@ -1,7 +1,6 @@
 require_relative '../../spec_helper'
 
 describe VagrantPlugins::DevCommands::Help do
-  definer  = VagrantPlugins::DevCommands::Definer
   help     = VagrantPlugins::DevCommands::Help
   registry = VagrantPlugins::DevCommands::Registry
 
@@ -11,22 +10,22 @@ describe VagrantPlugins::DevCommands::Help do
     end
 
     it 'displays a list of available commands' do
-      definer.define 'bar', 'bar'
-      definer.define 'foo', 'foo'
-
       reg          = registry.new
-      reg.commands = definer.commands
+      reg.commands = {
+        'bar' => { command: 'bar' },
+        'foo' => { command: 'foo' }
+      }
 
       expect { help.display(reg) }.to output(/bar/).to_stdout
       expect { help.display(reg) }.to output(/foo/).to_stdout
     end
 
     it 'display description if available' do
-      definer.define 'bar', 'bar'
-      definer.define 'foo', desc: 'has a description', command: 'foo'
-
       reg          = registry.new
-      reg.commands = definer.commands
+      reg.commands = {
+        'bar' => { command: 'bar' },
+        'foo' => { command: 'foo', desc: 'has a description' }
+      }
 
       expect { help.display(reg) }.to output(/has a description/).to_stdout
     end
