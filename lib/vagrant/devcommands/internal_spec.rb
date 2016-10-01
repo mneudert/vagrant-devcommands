025ec134066224a40a80967a5afef879ee5478ae
Marc Neudert <marc.neudert@gmail.com>
2016-10-01 16:59:48 +0200

    improves prefix-usage for locale strings

--- lib/vagrant/devcommands/internal_spec.rb
+++ lib/vagrant/devcommands/internal_spec.rb
@@ -2,18 +2,20 @@ module VagrantPlugins
   module DevCommands
     # Contains the specs for internal commands
     class InternalSpec
+      I18N_KEY = 'vagrant_devcommands.internal'.freeze
+
       HELP = {
-        desc:  I18n.t('vagrant_devcommands.internal.help.desc'),
+        desc:  I18n.t("#{I18N_KEY}.help.desc"),
         name:  'help',
         usage: 'vagrant run %{command} [command]',
-        help:  I18n.t('vagrant_devcommands.internal.help.help')
+        help:  I18n.t("#{I18N_KEY}.help.help")
       }.freeze
 
       VERSION = {
-        desc:  I18n.t('vagrant_devcommands.internal.version.desc'),
+        desc:  I18n.t("#{I18N_KEY}.version.desc"),
         name:  'version',
         usage: 'vagrant run %{command}',
-        help:  I18n.t('vagrant_devcommands.internal.version.help')
+        help:  I18n.t("#{I18N_KEY}.version.help")
       }.freeze
     end
   end
