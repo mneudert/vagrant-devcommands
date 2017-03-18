65c19185e7642c01e5995317a0b661d912619eec
Marc Neudert <marc.neudert@gmail.com>
2017-03-18 13:51:19 +0100

    ensures argv box passing/precedence

--- test/acceptance/test.sh
+++ test/acceptance/test.sh
@@ -7,6 +7,11 @@ cd ${0%/*}
 export BUNDLE_GEMFILE='../../Gemfile'
 export VAGRANT_I_KNOW_WHAT_IM_DOING_PLEASE_BE_QUIET='true'
 
+# utility functions
+trim() {
+  sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' <<< $1
+}
+
 # prepare tests
 echo 'Installing dependencies...'
 bundle install > /dev/null
@@ -16,5 +21,29 @@ bundle exec vagrant up > /dev/null
 
 # run tests
 echo 'Running tests...'
-bundle exec vagrant run primary hostname
-bundle exec vagrant run secondary hostname
+
+result=$(trim "$(bundle exec vagrant run hostname)")
+
+[[ 'primary.vagrant.devcommands' == $result ]] || {
+  echo 'Default box not used...'
+  echo "Got result: '${result}'"
+  exit 1
+}
+
+result=$(trim "$(bundle exec vagrant run primary hostname)")
+
+[[ 'primary.vagrant.devcommands' == $result ]] || {
+  echo 'Passing default box as argv not working...'
+  echo "Got result: '${result}'"
+  exit 1
+}
+
+result=$(trim "$(bundle exec vagrant run secondary hostname)")
+
+[[ 'secondary.vagrant.devcommands' == $result ]] || {
+  echo 'Box passed using argv not taking precedence over configuration...'
+  echo "Got result: '${result}'"
+  exit 1
+}
+
+echo 'Success!'
