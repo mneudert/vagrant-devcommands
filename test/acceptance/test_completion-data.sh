37d0ccd0923b9f219eef8e9cac17e12603b5573a
Marc Neudert <marc.neudert@gmail.com>
2019-05-23 20:33:00 +0200

    Fix result counter for completion-data test

--- test/acceptance/test_completion-data.sh
+++ test/acceptance/test_completion-data.sh
@@ -1,6 +1,6 @@
 result=$(trim "$(bundle exec vagrant run completion-data | wc -w)")
 
-[[ "${result}" == '15' ]] || {
+[[ "${result}" == '19' ]] || {
   echo 'Completion data contains unexpected number of return values...'
   echo "Got result: '${result}'"
   exit 1
