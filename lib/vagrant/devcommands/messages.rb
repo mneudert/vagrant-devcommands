629759e4c3d04b096acbca43c82b78bb187cef39
Marc Neudert <marc.neudert@gmail.com>
2024-09-15 12:18:21 +0200

    Remove redundant block calls

--- lib/vagrant/devcommands/messages.rb
+++ lib/vagrant/devcommands/messages.rb
@@ -8,39 +8,39 @@ module VagrantPlugins
     class Messages
       I18N_KEY = 'vagrant_devcommands.message'
 
-      def self.chain_no_help(&out)
-        out.call I18n.t("#{I18N_KEY}.chain_no_help")
+      def self.chain_no_help
+        yield I18n.t("#{I18N_KEY}.chain_no_help")
       end
 
-      def self.command_alias_no_help(&out)
-        out.call I18n.t("#{I18N_KEY}.command_alias_no_help")
+      def self.command_alias_no_help
+        yield I18n.t("#{I18N_KEY}.command_alias_no_help")
       end
 
-      def self.command_no_help(&out)
-        out.call I18n.t("#{I18N_KEY}.command_no_help")
+      def self.command_no_help
+        yield I18n.t("#{I18N_KEY}.command_no_help")
       end
 
-      def self.missing_commandfile(&out)
-        out.call I18n.t("#{I18N_KEY}.missing_commandfile")
+      def self.missing_commandfile
+        yield I18n.t("#{I18N_KEY}.missing_commandfile")
       end
 
-      def self.no_commands(&out)
-        out.call I18n.t("#{I18N_KEY}.no_commands")
+      def self.no_commands
+        yield I18n.t("#{I18N_KEY}.no_commands")
       end
 
-      def self.plugin_readme(&out)
+      def self.plugin_readme
         curdir = File.expand_path(__dir__)
         readme = File.expand_path(File.join(curdir, '../../../README.md'))
 
-        out.call I18n.t("#{I18N_KEY}.plugin_readme", readme: readme)
+        yield I18n.t("#{I18N_KEY}.plugin_readme", readme: readme)
       end
 
-      def self.plugin_usage(&out)
-        out.call I18n.t("#{I18N_KEY}.plugin_usage")
+      def self.plugin_usage
+        yield I18n.t("#{I18N_KEY}.plugin_usage")
       end
 
       def self.pre_ln(msg, &out)
-        out.call ''
+        yield ''
 
         public_send(msg, &out)
       end
